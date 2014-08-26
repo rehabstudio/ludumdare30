b22e0c062d2796881bd32f9735f31174fb35e9f7
unknown <neil@rehabstudio.com>
2014-08-26 02:35:49 +0100

    Add instructions

--- static/js/src/strings.js
+++ static/js/src/strings.js
@@ -3,7 +3,7 @@ module.exports = {
     startPrompt: 'Press Start',
     readyPrompt: 'Ready',
     enteringLevel: 'Entering Sector',
-    copyText: '(C) 2014 by @DesignThenCode and @NJMCode. Ludum Dare 30',
+    copyText: 'WASD: move ship, mouse: aim/shoot, SPACE: use wormhole\n\n(C) 2014 by @DesignThenCode and @NJMCode. Ludum Dare 30',
     gameOver: 'Game Over'
 
 };
